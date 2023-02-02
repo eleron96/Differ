6a85ca181988c1fd183c1e742141a6519a8eb425
nikogamsahurdia <eleron96@gmail.com>
2023-02-02 21:39:40 +0300

    changed gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,13 @@
+import argparse
+
+
 def main():
-    print('Hello, World!')
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    print(args)
+
 
 if __name__ == '__main__':
     main()
