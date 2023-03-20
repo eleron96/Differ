d3e8decc123222f7e40d0089af6f7788f8951b7e
nikogamsahurdia <eleron96@gmail.com>
2023-03-21 00:06:17 +0300

    diff stylish corrected 2

--- gendiff/scripts/diff_stylish.py
+++ gendiff/scripts/diff_stylish.py
@@ -95,7 +95,7 @@ def compare_dicts(d1, d2, indent_level, indent):
             result.extend(handle_both_keys(d1, d2, key, indent_level, indent))
         elif key in d1:
             result.extend(handle_key_in_d1(d1, key, indent_level, indent))
-        elif key in d2:
+        else:
             result.extend(handle_key_in_d2(d2, key, indent_level, indent))
 
     return result
@@ -103,52 +103,29 @@ def compare_dicts(d1, d2, indent_level, indent):
 
 def handle_both_keys(d1, d2, key, indent_level, indent):
     result = []
-    indent_str = ' ' * (indent * indent_level)
 
-    if isinstance(d1[key], dict) and isinstance(d2[key], dict):
-        result.append(f"{indent_str}{key}: {{")
+    if d1[key] == d2[key]:
+        result.append(indent * indent_level + f'  {key}: {d1[key]}')
+    elif isinstance(d1[key], dict) and isinstance(d2[key], dict):
+        result.append(indent * indent_level + f'  {key}:')
         result.extend(compare_dicts(d1[key], d2[key], indent_level + 1, indent))
-        result.append(f"{indent_str}}}")
-    elif d1[key] != d2[key]:
-        result.append(f"{indent_str}- {key}: {d1[key]}")
-        result.append(f"{indent_str}+ {key}: {d2[key]}")
     else:
-        result.append(f"{indent_str}  {key}: {d1[key]}")
+        result.append(indent * indent_level + f'- {key}: {d1[key]}')
+        result.append(indent * indent_level + f'+ {key}: {d2[key]}')
 
     return result
 
 
 def handle_key_in_d1(d1, key, indent_level, indent):
-    result = []
-    indent_str = ' ' * (indent * indent_level)
-
-    if isinstance(d1[key], dict):
-        result.append(f"{indent_str}- {key}: {{")
-        result.extend(compare_dicts(d1[key], {}, indent_level + 1, indent))
-        result.append(f"{indent_str}}}")
-    else:
-        result.append(f"{indent_str}- {key}: {d1[key]}")
-
-    return result
+    return [indent * indent_level + f'- {key}: {d1[key]}']
 
 
 def handle_key_in_d2(d2, key, indent_level, indent):
-    result = []
-    indent_str = ' ' * (indent * indent_level)
-
-    if isinstance(d2[key], dict):
-        result.append(f"{indent_str}+ {key}: {{")
-        result.extend(compare_dicts({}, d2[key], indent_level + 1, indent))
-        result.append(f"{indent_str}}}")
-    else:
-        result.append(f"{indent_str}+ {key}: {d2[key]}")
-
-    return result
+    return [indent * indent_level + f'+ {key}: {d2[key]}']
 
 
-def compare_files_stylish(file1, file2, indent=4):
-    data1 = load_data(file1)
-    data2 = load_data(file2)
-
-    result = compare_dicts(data1, data2, 0, indent)
-    return '\n'.join(result)
+def compare_files_stylish(file_path1, file_path2):
+    d1 = load_data(file_path1)
+    d2 = load_data(file_path2)
+    result_lines = compare_dicts(d1, d2, 0, '    ')
+    return '{\n' + '\n'.join(result_lines) + '\n}'
