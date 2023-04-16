a5c4fdb69de42c013e0b8e99fdb1dddb6e7264c2
nikogamsahurdia <eleron96@gmail.com>
2023-04-16 16:46:29 +0300

    refactoring 5

--- gendiff/formaters/diff_plain.py
+++ gendiff/formaters/diff_plain.py
@@ -24,13 +24,19 @@ def render(tree, path_key=""):
     node_type = tree['type']
     if node_type == 'added':
         value = stringify(tree['value'])
-        result.append(f"Property '{path_key}' was added with value: {value}")
+        if value == '[complex value]':
+            result.append(f"Property '{path_key}' was added with value: {value}")
+        else:
+            result.append(f"Property '{path_key}' was added with value: '{value}'")
     elif node_type == 'removed':
         result.append(f"Property '{path_key}' was removed")
     elif node_type == 'changed':
         value_1 = stringify(tree['value_1'])
         value_2 = stringify(tree['value_2'])
-        result.append(f"Property '{path_key}' was updated. From {value_1} to {value_2}")
+        if value_1 == '[complex value]':
+            result.append(f"Property '{path_key}' was updated. From {value_1} to '{value_2}'")
+        else:
+            result.append(f"Property '{path_key}' was updated. From '{value_1}' to '{value_2}'")
     elif node_type == 'no_changes':
         pass
     return result
